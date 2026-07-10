24529355cb675914469e522275e1ec7629653eca
Jacques Massey <jacques.massey@outlook.com>
2026-07-10 10:19:50 +0100

    Structural improvement

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ bootstrap = Bootstrap4(app)
 def home():
     return render_template('home.html')
 
+@app.route("/spotify")
+def spotify():
+    return render_template('spotify.html')
+
+@app.route("/learn")
+def learn():
+    return render_template('learn.html')
+
+@app.route("/keygen")
+def keygen():
+    return render_template('keygen.html')
+
+@app.route("/treedentify")
+def treedentify():
+    return render_template('treedentify.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
