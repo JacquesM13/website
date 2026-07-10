40ee63dcde3709a3945b997be6f159baf317a9fa
Jacques Massey <jacques.massey@outlook.com>
2026-07-10 09:37:20 +0100

    Format homepage

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask_bootstrap import Bootstrap4
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap4(app)
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
